ca1be7179ba6a362b9afccbd65d8e34ed2a9756e
Ndhlovu1 <tinomudaishendhlovu@gmail.com>
2026-03-15 20:41:31 +0200

    Added (OOP BASICS)

---
+++ Program.cs
@@ -0,0 +1,74 @@
+﻿using System;
+
+public class Program
+{
+
+
+public class Animal
+{
+
+    public virtual void makeSound ()
+    {
+        Console.WriteLine("Animal makes a sound");
+    }
+
+    public interface IAnimal
+    {
+        public void Eat();
+    }
+
+    
+}
+
+
+public class Dog : Animal
+{
+
+    public override void makeSound()
+    {
+        Console.WriteLine("Barking");
+    }
+
+    public void Eat()
+    {
+        Console.WriteLine("Dog is eating");
+    }
+    
+
+}
+
+
+public class Cat : Animal
+{
+
+    public override void makeSound()
+    {
+        Console.WriteLine("Meowing");
+    }
+
+    public void Eat()
+    {
+        Console.WriteLine("Cat is eating");
+    }
+    
+}
+
+
+
+
+
+
+public static void Main(string[] args)
+    {
+        Console.WriteLine("Calling Dog to Eat");
+        Dog dog = new Dog();
+        dog.Eat();
+        
+    }
+
+
+    
+}
+
+
+
